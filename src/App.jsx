70e8898151ddfff03bf712934fad9fc85831987e
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-04-27 12:50:17 +0000

    setup project structure folder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// --------------- IMPORT FILES ------------------ //
+import 'bulma/css/bulma.css';
+import './app.scss';
+
+// ---------------  APP COMPONENT ---------------- //
+
+const App = ( ) => {
+    return <div id="shoping-list-app">Shoping List</div>
+}
+
+export default App;
