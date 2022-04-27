39a825fe0dcc21f8442a6c1fe46139b4629e3c61
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-04-27 13:38:21 +0000

    Shopping lists simple data for testing

---
+++ src/components/data/data.jsx
@@ -0,0 +1,29 @@
+let data = [
+    {
+        id: 1,
+        name: 'Marjane Shopping List',
+        total: 120,
+        date: 'Wed 27 Apr 18:00',
+        isReminder: true,
+        items: [
+            {
+                id: 1,
+                name: 'Spagitti 500g',
+                quantity: 4,
+                price: 500,
+                isBought: false,
+            },
+            {
+                id: 2,
+                name: 'Sidi Ali pack',
+                quantity: 1,
+                price: 500,
+                isBought: true,
+            }
+        ]
+    }
+]
+
+export function getLists () {
+    return data;
+}
