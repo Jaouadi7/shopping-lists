cba230f7f8fbe76381a03ac6f4e750b9331489c4
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-04-28 04:29:03 +0000

    Display clicked shopping list items data

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,39 @@
+import { useParams } from 'react-router-dom';
+import { ImCheckmark } from "react-icons/im";
 import './list.scss';
+import { getLists } from '../../data/Data';
+
 
 const List = () => {
+
+  const { id } = useParams();
+  const shoppingLists = getLists();
+  const currentList = shoppingLists.filter( list => (list.id === Number(id) ))
   return (
-    <div>List</div>
+    <div className="shopping-list">
+      <h1>{currentList[0].name}</h1>
+      <ul>
+        {
+          currentList[0].items.map( ( list ) => {
+            return (
+              <li key={list.id}>
+                <div className="shopping-list_header">
+                      <h2 className="shopping-item_heading">{ list.name } (x{list.quantity})</h2>
+                      <div className="shopping-item_control">
+                        <ImCheckmark />
+                      </div>
+                    </div>
+                    <div className="shopping-list_details">
+                      <h4 className='shopping-list_total'>Total: { list.price }DH</h4>
+                    </div>
+              </li>
+            )
+          })
+        }
+        
+      </ul>
+    </div>
+   
   )
 }
 
