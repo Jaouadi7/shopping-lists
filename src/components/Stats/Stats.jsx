cda88f93550a894217c29f1901e184aa0e329e21
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-04-29 11:37:52 +0000

    Update Data file, add stats component and apply some css styles

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,29 @@
+import './stats.scss';
+
+const Stats = () => {
+  return (
+    <div className='shopping-lists_stats'>
+        <h2 className='title app-title mb-4'>You Spent:</h2>
+        <div className="stats-details is-flex is-justify-content-space-between">
+            <div className="stats-item today box is-shadowless p-4 mb-3">
+                <h3>Today</h3>
+                <h4>1040,00 DH</h4>
+            </div>
+            <div className="stats-item yesterday  box is-shadowless p-4 mb-3">
+                <h3>Yesterday</h3>
+                <h4>1040,00 DH</h4>
+            </div>
+            <div className="stats-item week  box is-shadowless p-4 mb-3">
+                <h3>This Week</h3>
+                <h4>1040,00 DH</h4>
+            </div>
+            <div className="stats-item month  box is-shadowless p-4 mb-3">
+                <h3>This Month</h3>
+                <h4>1040,00 DH</h4>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Stats
--- src/pages/lists/Lists.jsx
+++ src/pages/lists/Lists.jsx
@@ -1,38 +1,46 @@
-import { ImFileEmpty, ImCheckmark } from "react-icons/im";
+import { ImFileEmpty, ImCheckmark, ImPencil } from "react-icons/im";
 import { Link } from 'react-router-dom';
 import './lists.scss';
+import Stats from "../../components/Stats/Stats";
 
 
 const Lists = ( { lists } ) => {
   return (
-    <ul className="shopping-lists">
-      {
-        lists.map( ( list ) => {
-          return  <li key={ list.id } className="shopping-list_item pb-2 mb-2">
-                    <div className="shopping-list_header is-flex is-justify-content-space-between is-align-content-center">
-                      <h2 className="shopping-list_heading">
-                        <Link to={`/list/${list.id}`} className="has-text-black">
-                          <span className="icon">
-                            <ImFileEmpty className="mr-2" />
+    <>
+      <ul className="shopping-lists">
+        {
+          lists.map( ( list ) => {
+            return  <li key={ list.id } className={ `shopping-list_item pb-2 mb-2 ${list.isCompleted && 'completed'}`} >
+                      <div className="shopping-list_header is-flex is-justify-content-space-between is-align-content-center">
+                        <h2 className="shopping-list_heading">
+                          <Link to={`/list/${list.id}`} className="has-text-black">
+                            <span className="icon">
+                              <ImFileEmpty className="mr-2" />
+                            </span>
+                            <span>{ list.name }</span>
+                          </Link>
+                        </h2>
+                        <div className="shopping-list_control">
+                        <span className="icon mr-2">
+                            <ImPencil  />
                           </span>
-                          <span>{ list.name }</span>
-                        </Link>
-                      </h2>
-                      <div className="shopping-list_control">
-                        <span className="icon">
-                          <ImCheckmark  />
-                        </span>
+                          <span className="icon isCompleted">
+                            <ImCheckmark  />
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                    <div className="shopping-list_details is-flex">
-                      <h4 className='shopping-list_total mr-4'>Total: { list.total }DH</h4>
-                      <h4 className='shopping-list_date'>Date: { list.date } </h4>
-                    </div>
-                  </li> 
-        })
-      }
-      
-    </ul>
+                      <div className="shopping-list_details is-flex">
+                        <h4 className='shopping-list_total mr-4'>Total: { list.total }DH</h4>
+                        <h4 className='shopping-list_date'>Date: { list.date } </h4>
+                      </div>
+                    </li> 
+          })
+        }
+        
+      </ul>
+      <Stats />
+    </>
+    
   )
 }
 
