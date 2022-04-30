9a4723519ecdc6f3bf01789349cd3ca37a771786
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-04-30 18:56:08 +0000

    add container

--- src/pages/no-lists/Welcome.jsx
+++ src/pages/no-lists/Welcome.jsx
@@ -4,19 +4,25 @@ import { ImArrowRight2 } from 'react-icons/im';
 
 const Welcome = () => {
   return (
-    <div className='shopping-lists_welcome is-flex is-flex-direction-column is-align-items-center is-justify-content-center'>
-      <figure className="image  is-48x48">
-        <img src={Logo} alt="Shopping List" />
-      </figure>
-      <h1 className='shopping-lists_title is-size-2'>Shopping List</h1>
-      <h3 className='subtitle'>Make your shopping More easier</h3>
-      <Link className='button has-no-border has-text-weight-semibold' to={'/creat-shoping-list'}>
-        <span>Let's create a list</span>
-        <span className='icon'>
-          <ImArrowRight2 />
-        </span>
-      </Link>
-    </div>
+    <>
+      <div className="container">
+        <div className="p-4">
+          <div className='shopping-lists_welcome is-flex is-flex-direction-column is-align-items-center is-justify-content-center'>
+            <figure className="image  is-48x48">
+              <img src={Logo} alt="Shopping List" />
+            </figure>
+            <h1 className='shopping-lists_title is-size-2'>Shopping List</h1>
+            <h3 className='subtitle'>Make your shopping More easier</h3>
+            <Link className='button has-no-border has-text-weight-semibold' to={'/creat-shoping-list'}>
+              <span>Let's create a list</span>
+              <span className='icon'>
+                <ImArrowRight2 />
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
