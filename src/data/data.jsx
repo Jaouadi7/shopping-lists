2a95f4b2e786910a042bcd272d8207465e2b5fac
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-05-04 00:51:51 +0000

    empty data

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,54 +1,8 @@
+// START WITH EMPTY DATA
 let data = [
-    {
-        id: 1,
-        name: 'Marjane Shopping List',
-        total: 120,
-        date: 'Wed 27 Apr 18:00',
-        isReminder: true,
-        isCompleted: false,
-        items: [
-            {
-                id: 1,
-                name: 'Spagitti 500g',
-                quantity: 4,
-                price: 500,
-                isBought: false,
-            },
-            {
-                id: 2,
-                name: 'Sidi Ali pack',
-                quantity: 1,
-                price: 500,
-                isBought: true, 
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: 'BIM Shopping List',
-        total: 120,
-        date: 'Wed 27 Apr 18:00',
-        isReminder: true,
-        isCompleted: true,
-        items: [
-            {
-                id: 1,
-                name: 'Spagitti 500g',
-                quantity: 4,
-                price: 500,
-                isBought: false,
-            },
-            {
-                id: 2,
-                name: 'Sidi Ali pack',
-                quantity: 1,
-                price: 500,
-                isBought: true, 
-            }
-        ]
-    }
+
 ]
 
-export function getLists () {
+export function getLists() {
     return data;
 }
